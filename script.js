7d09c7ba57c4914edb7c1ffbdedabaa95eb49add
Richard-Younes <richard.younes@lau.edu>
2024-01-16 14:28:50 +0200

    Added the Menu fade animation

--- script.js
+++ script.js
@@ -2,12 +2,18 @@
 
 'use strict';
 
+const nav = document.querySelector('.nav');
+
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
 ///////////////////////////////////////
 // ************************ Modal window ************************//
 
@@ -86,9 +92,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 });
 
 // ************************ Creating a Tabbed component ************************ //
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener('click', function (e) {
 	const clicked = e.target.closest('.operations__tab');
@@ -105,6 +108,26 @@ tabsContainer.addEventListener('click', function (e) {
 	clicked.classList.add('operations__tab--active');
 	document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 });
+
+// *************** Menu fade animation ***************//
+const handleHover = function (e) {
+	if (e.target.classList.contains('nav__link')) {
+		const link = e.target;
+		const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+		const logo = link.closest('.nav').querySelector('img');
+
+		siblings.forEach(el => {
+			if (el !== link) {
+				el.style.opacity = this;
+			}
+			logo.style.opacity = this;
+		});
+	}
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 // ************************ Selecting elements ************************//
 
