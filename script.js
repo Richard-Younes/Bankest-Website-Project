f1b2ef052165fcaf92086bb802ea7397a3b0c31d
Richard-Younes <richard.younes@lau.edu>
2024-01-17 13:42:12 +0200

    added the Revealing element on scroll animation

--- script.js
+++ script.js
@@ -162,7 +162,6 @@ const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
 const stickyNav = function (entries) {
 	const [entry] = entries;
-	console.log(entry);
 	if (!entry.isIntersecting) {
 		nav.classList.add('sticky');
 	} else {
@@ -178,6 +177,29 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 	rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+// ************************ Reveal Section ************************ //
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+	const [entry] = entries;
+	console.log(entry);
+	if (!entry.isIntersecting) return;
+	entry.target.classList.remove('section--hidden');
+
+	observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+	root: null,
+	threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+	sectionObserver.observe(section);
+	section.classList.add('section--hidden');
+});
+
 // ************************ Selecting elements ************************//
 
 // const header = document.querySelector('.header');
